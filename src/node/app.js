774ede885e3e675c5ffe3b6f169d9eedc5d2783d
zhuang13 <zbob09@163.com>
2018-08-19 02:35:17 +0800

    ok

--- src/node/app.js
+++ src/node/app.js
@@ -83,17 +83,15 @@ const server = (req, resp) => {
     });
 }
 
-setTimeout(() => {
-    let appServer = app.createServer(server);
-    appServer.listen(config.PORT);
-    console.log(`open in ${config.HOST}`);
-    
-    process.on('SIGINT', () => {
-        console.log('Closing server...');
-        appServer.close(() => {
-            console.log('Server closed !!! ');
-            exec('sh bin/moveStatic.sh', function(err,stdout,stderr){console.log('moveStatic.sh', err, stdout,stderr)});
-            process.exit();
-        });
+let appServer = app.createServer(server);
+appServer.listen(config.PORT);
+console.log(`open in ${config.HOST}`);
+
+process.on('SIGINT', () => {
+    console.log('Closing server...');
+    appServer.close(() => {
+        console.log('Server closed !!! ');
+        exec('sh bin/moveStatic.sh', function(err,stdout,stderr){console.log('moveStatic.sh', err, stdout,stderr)});
+        process.exit();
     });
-}, 3000);
+});
